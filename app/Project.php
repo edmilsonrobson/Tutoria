631a9dee5ac189cbc7758bd60950104c9384dd90
Edmilson Rocha <edmilsonrobson@gmail.com>
2017-12-19 21:37:13 -0300

    Refactored project's student average score method

--- app/Project.php
+++ app/Project.php
@@ -5,28 +5,24 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Project extends Model
-{
+class Project extends Model {
     use SoftDeletes;
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
     public function participants() {
         return $this->belongsToMany(User::class)->withTimestamps();
     }
 
-
     public function studentsAverageScore() {
-        $participants = $this->participants;
+        $participants = $this->participants()->with('student')->get();
 
-        $sum = 0;
-        $totalStudents = 0;
-        foreach($participants as $participant) {
-            if ($participant->isStudent()) {
-                $totalStudents++;
-                $sum += $participant->student->lastRating()->averageScore();
-            }
-        }
-
-        return $sum / $totalStudents;
+        return $participants->filter(function ($participant) {
+            return $participant->isStudent();
+        })->average(function ($participant) {
+            return $participant->student->lastRating()->averageScore();
+        });
     }
 
 }
